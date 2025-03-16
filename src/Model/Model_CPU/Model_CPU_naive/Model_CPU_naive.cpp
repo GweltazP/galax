fdf4605d5308080353127999e13ccd59b8d3485f
PC-TAZ\gwelt <gweltaz-poussin@orange.fr>
2025-03-16 13:35:06 +0100

    Version fonctionnelle du Barnes-Hut (sur mon PC), à tester sur les PC de l'école

--- src/Model/Model_CPU/Model_CPU_naive/Model_CPU_naive.cpp
+++ src/Model/Model_CPU/Model_CPU_naive/Model_CPU_naive.cpp
@@ -1,76 +1,120 @@
-#ifdef GALAX_MODEL_CPU_FAST
-
 #include <cmath>
-#include <vector>
-#include <memory>
+
 #include "Model_CPU_naive.hpp"
+#include <iostream>
+#include <xsimd/xsimd.hpp>
 #include <omp.h>
 
-// Insère une particule dans l'Octree
-void Octree::insert_particle(int index, const Particles& particles, const std::vector<float>& masses) {
-    insert_recursive(root.get(), index, particles, masses);
+namespace xs = xsimd;
+using b_type = xs::batch<float, xs::avx2>;
+
+void Octree::delete_tree(OctreeNode* node) {
+    if (!node) return;
+    for (int i = 0; i < 8; i++) {
+        delete_tree(node->children[i]);
+    }
+    delete node;
 }
 
-void Octree::insert_recursive(OctreeNode* node, int index, const Particles& particles, const std::vector<float>& masses) {
+void Octree::insert_particle(int index, const Particles& particles, const Initstate& initstate) {
+    insert_recursive(root, index, particles, initstate);
+}
+
+void Octree::insert_recursive(OctreeNode* node, int index, const Particles& particles, const Initstate& initstate) {
     float px = particles.x[index];
     float py = particles.y[index];
     float pz = particles.z[index];
-
     if (node->particle_index == -1 && node->children[0] == nullptr) {
         node->particle_index = index;
         node->x = px;
         node->y = py;
         node->z = pz;
-        node->mass = masses[index];
+        node->mass = initstate.masses[index];
     } else {
         if (node->children[0] == nullptr) {
             float halfSize = node->size / 2.0f;
-            for (int i = 0; i < 8; i++) {
-                float offsetX = (i & 1) ? halfSize : -halfSize;
-                float offsetY = (i & 2) ? halfSize : -halfSize;
-                float offsetZ = (i & 4) ? halfSize : -halfSize;
-                node->children[i] = std::make_unique<OctreeNode>(node->x + offsetX, node->y + offsetY, node->z + offsetZ, halfSize);
-            }
+            node->subdivide();
             int oldIndex = node->particle_index;
             node->particle_index = -1;
-            insert_recursive(root.get(), oldIndex, particles, masses);
+            // insert_recursive(root, oldIndex, particles, initstate.masses[index]);
         }
         int childIndex = (px > node->x) | ((py > node->y) << 1) | ((pz > node->z) << 2);
-        insert_recursive(node->children[childIndex].get(), index, particles, masses);
-        node->mass += masses[index];
-        node->x = (node->x * node->mass + px * masses[index]) / (node->mass + masses[index]);
-        node->y = (node->y * node->mass + py * masses[index]) / (node->mass + masses[index]);
-        node->z = (node->z * node->mass + pz * masses[index]) / (node->mass + masses[index]);
+        insert_recursive(node->children[childIndex], index, particles, initstate);
+    
+        node->x = (node->x * node->mass + px * initstate.masses[index]) / (node->mass + initstate.masses[index]);
+        node->y = (node->y * node->mass + py * initstate.masses[index]) / (node->mass + initstate.masses[index]);
+        node->z = (node->z * node->mass + pz * initstate.masses[index]) / (node->mass + initstate.masses[index]);
+        node->mass += initstate.masses[index];
     }
 }
 
-void Octree::compute_force(int index, float& ax, float& ay, float& az, const Particles& particles, const std::vector<float>& masses) {
-    compute_force_recursive(root.get(), index, ax, ay, az, particles, masses);
+void Octree::compute_force(int index, std::vector<float>& ax, std::vector<float>& ay, std::vector<float>& az, const Particles& particles, const Initstate& initstate) {
+    compute_force_recursive(root, index, ax, ay, az, particles, initstate);
 }
 
-void Octree::compute_force_recursive(OctreeNode* node, int index, float& ax, float& ay, float& az, const Particles& particles, const std::vector<float>& masses) {
-    if (!node || (node->particle_index == index && node->particle_index != -1)) {
-        return;
+void Octree::compute_force_recursive(OctreeNode* node, int index, std::vector<float>& ax, std::vector<float>& ay, std::vector<float>& az, const Particles& particles, const Initstate& initstate) {
+    // std::cout << "Calcul récursif sur la particule " << index << std::endl;
+	// std::cout << "Index du nœud courrant " << (node->particle_index) << std::endl;
+	// std::cout << "Acceleration x/y/z " << ax[index] << "/" << ay[index] << "/" << az[index] << std::endl;
+	if (!node || (node->particle_index == index) || (node->mass == 0)) {
+        // std::cout << "La particule n'applique pas de force sur elle même " << std::endl;
+		// std::cout << std::endl;
+		return;
     }
+
+	// std::cout << "x " << particles.x[index] << std::endl;
+    // std::cout << "y " << particles.y[index] << std::endl;
+    // std::cout << "z " << particles.z[index] << std::endl;
+	// std::cout << "Centre du noeud courrant :" << (node->center_x) << "/" << (node->center_y) << "/" << (node->center_y) << "/" << (node->size) << std::endl;
+
+	if(node->contains(particles.x[index], particles.y[index], particles.z[index]) && (node->children[0] != nullptr)){
+		// std::cout << "La particule " << index <<" est dans le nœud" << std::endl;
+		// std::cout << std::endl;
+		for (int i = 0; i < 8; i++) {
+            compute_force_recursive(node->children[i], index, ax, ay, az, particles, initstate);
+        }
+	}
+
+	// std::cout << " CoM_x " << node->x << std::endl;
+	// std::cout << " CoM_y " << node->y << std::endl;
+	// std::cout << " CoM_z " << node->z << std::endl;
+	// std::cout << " Masse " << node->mass << std::endl;
 
     float dx = node->x - particles.x[index];
     float dy = node->y - particles.y[index];
     float dz = node->z - particles.z[index];
     float dist2 = dx * dx + dy * dy + dz * dz;
-    
-    if (dist2 < 1e-4) {
-        return;
+
+	// std::cout << " dx " << dx << std::endl;
+	// std::cout << " dy " << dy << std::endl;
+	// std::cout << " dz " << dz << std::endl;
+	// std::cout << " distance carré " << dist2 << std::endl;
+
+
+    if (dist2 < 1.0) {
+        dist2 = 10.0;
     }
 
     float dist = std::sqrt(dist2);
-    if (node->size / dist < theta || node->children[0] == nullptr) {
-        float force = node->mass / (dist2 * dist);
-        ax += dx * force;
-        ay += dy * force;
-        az += dz * force;
+
+	// std::cout << " distance " << dist << std::endl;
+	// std::cout << " taille " << node->size << std::endl;
+	// std::cout << " ratio " << (node->size / dist) << std::endl;
+
+    if ((node->size / dist) < theta || node->children[0] == nullptr) {
+        // std::cout << " On approxime au centre de masse " << std::endl;
+		float force = 10.0 * (1/(dist * dist * dist));
+        ax[index] += node->mass * dx * force;
+        ay[index] += node->mass * dy * force;
+        az[index] += node->mass * dz * force;
+        // std::cout << " force " << node->mass * dist << std::endl;
+        // std::cout << " acceleration x/y/z " << ax[index] << "/" << ay[index] << "/" << az[index] << std::endl;
+		// std::cout << std::endl;
     } else {
-        for (const auto& child : node->children) {
-            compute_force_recursive(child.get(), index, ax, ay, az, particles, masses);
+		// std::cout << " On approxime pas au centre de masse " << std::endl;
+		// std::cout << std::endl;
+        for (int i = 0; i < 8; i++) {
+            compute_force_recursive(node->children[i], index, ax, ay, az, particles, initstate);
         }
     }
 }
@@ -80,7 +124,12 @@ Model_CPU_naive::Model_CPU_naive(const Initstate& initstate, Particles& particle
 
 void Model_CPU_naive::step() {
 
-    float max_x, max_y, max_z, min_x, min_y, min_z = 0.0;
+    float max_x = -std::numeric_limits<float>::infinity();
+    float max_y = -std::numeric_limits<float>::infinity();
+    float max_z = -std::numeric_limits<float>::infinity();
+    float min_x = std::numeric_limits<float>::infinity();
+    float min_y = std::numeric_limits<float>::infinity();
+    float min_z = std::numeric_limits<float>::infinity();
 
     for (int i = 0; i < n_particles; i++) {
         if(particles.x[i] > max_x) max_x = particles.x[i];
@@ -91,19 +140,24 @@ void Model_CPU_naive::step() {
         if(particles.z[i] < min_x) min_z = particles.z[i];
     }
 
+    float dist = std::sqrt(std::pow(max_x-min_x, 2) + std::pow(max_y-min_y, 2) + std::pow(max_z-min_z, 2));
+    // std::cout << "dist" << dist << std::endl;
     Octree tree(std::sqrt(std::pow(max_x-min_x, 2) + std::pow(max_y-min_y, 2) + std::pow(max_z-min_z, 2))); // MODIFIER TAILLE
+
     
     for (int i = 0; i < n_particles; i++) {
-        tree.insert_particle(i, particles, initstate.masses);
+        tree.insert_particle(i, particles, initstate);
     }
 
-    #pragma omp parallel for
+    // #pragma omp parallel for
     for (int i = 0; i < n_particles; i++) {
-        float ax = 0, ay = 0, az = 0;
-        tree.compute_force(i, ax, ay, az, particles, initstate.masses);
-        accelerationsx[i] = ax;
-        accelerationsy[i] = ay;
-        accelerationsz[i] = az;
+        accelerationsx[i] = 0;
+        accelerationsy[i] = 0;
+        accelerationsz[i] = 0;
+        tree.compute_force(i, accelerationsx, accelerationsy, accelerationsz, particles, initstate);
+        // std::cout << "############" << std::endl;
+        // std::cout << " acceleration totale x/y/z " << accelerationsx[i] << "/" << accelerationsy[i] << "/" << accelerationsz[i] << std::endl;
+        // std::cout << "############" << std::endl;      
     }
 
     for (int i = 0; i < n_particles; i++) {
@@ -115,5 +169,3 @@ void Model_CPU_naive::step() {
         particles.z[i] += velocitiesz[i] * 0.1f;
     }
 }
-
-#endif // GALAX_MODEL_CPU_FAST
--- src/Model/Model_CPU/Model_CPU_naive/Model_CPU_naive.hpp
+++ src/Model/Model_CPU/Model_CPU_naive/Model_CPU_naive.hpp
@@ -4,35 +4,66 @@
 #include "../Model_CPU.hpp"
 
 // Structure d'un nœud de l'Octree
+
 struct OctreeNode {
     float x, y, z;  // Centre de masse
+    float center_x, center_y, center_z; // Centre de la boite
     float mass;      // Masse totale du nœud
     float size;      // Taille de la boîte englobante
     int particle_index; // Indice de la particule si c'est une feuille (-1 sinon)
-    std::vector<std::unique_ptr<OctreeNode>> children;
+    OctreeNode* children[8];
     
-    OctreeNode(float _x, float _y, float _z, float _size)
-        : x(_x), y(_y), z(_z), size(_size), mass(0), particle_index(-1) {
-        children.resize(8, nullptr);
+    OctreeNode(float _x, float _y, float _z, float _size) : x(0), y(0), z(0), center_x(_x), center_y(_y), center_z(_z), size(_size), mass(0), particle_index(-1)
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            children[i] = nullptr;
+        }
     }
+
+    bool contains(float _x, float _y, float _z)
+    {
+        return (
+            _x >= x - size / 2 && _x <= x + size / 2 &&
+            _y >= y - size / 2 && _y <= y + size / 2 &&
+            _z >= z - size / 2 && _z <= z + size / 2
+        );
+    }
+
+    void subdivide()
+    {
+        float halfSize = size / 2.0f;
+        for (int i = 0; i < 8; i++)
+        {
+            float offsetX = (i & 1) ? halfSize : -halfSize;
+            float offsetY = (i & 2) ? halfSize : -halfSize;
+            float offsetZ = (i & 4) ? halfSize : -halfSize;
+            children[i] = new OctreeNode(center_x + offsetX, center_y + offsetY, center_z + offsetZ, halfSize);
+        };
+    };
 };
 
 // Classe Octree
 class Octree {
 public:
-    std::unique_ptr<OctreeNode> root;
+    OctreeNode* root;
     float theta = 0.5f; // Paramètre de précision
     
     Octree(float size) {
-        root = std::make_unique<OctreeNode>(0.0f, 0.0f, 0.0f, size);
+        root = new OctreeNode(0.0f, 0.0f, 0.0f, size);
     }
 
-    void insert_particle(int index, const Particles& particles, const std::vector<float>& masses);
-    void compute_force(int index, float& ax, float& ay, float& az, const Particles& particles, const std::vector<float>& masses);
+    ~Octree() {
+        delete_tree(root);
+    }
+
+    void insert_particle(int index, const Particles& particles, const Initstate& initstate);
+    void compute_force(int index, std::vector<float>& ax, std::vector<float>& ay, std::vector<float>& az, const Particles& particles, const Initstate& initstate);
 
 private:
-    void insert_recursive(OctreeNode* node, int index, const Particles& particles, const std::vector<float>& masses);
-    void compute_force_recursive(OctreeNode* node, int index, float& ax, float& ay, float& az, const Particles& particles, const std::vector<float>& masses);
+    void insert_recursive(OctreeNode* node, int index, const Particles& particles, const Initstate& initstate);
+    void compute_force_recursive(OctreeNode* node, int index, std::vector<float>& ax, std::vector<float>& ay, std::vector<float>& az, const Particles& particles, const Initstate& initstate);
+    void delete_tree(OctreeNode* node);
 };
 
 class Model_CPU_naive : public Model_CPU
